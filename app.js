8370d2d444879da6c6554fd4ee8ef0658a05ecc0
Jorge <jorge.alfmur@gmail.com>
2017-05-08 00:17:45 -0500

    npm install body-parser

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var express = require('express')
+const bodyParser = require('body-parser')
+const express = require('express')
 var app = express()
 
 //archivos static
 app.use(express.static('public'))
+
+//middleware para pasar datos de formularios
+app.use(bodyParser.json())
+//middleware para que acepte formato json
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //configuramos el motor de vista
 app.set('view engine', 'pug')
@@ -13,6 +19,12 @@ app.get('/', (req, res)=>{
 
 app.get('/login', (req, res)=>{
   res.render('login')
+})
+
+app.post('/users',(req, res)=>{
+	console.log(req.body.email)
+	console.log(req.body.password)
+	res.send("Recibimos tus datos")
 })
 
 app.listen(3000, ()=>{
