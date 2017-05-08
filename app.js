6a71357901dee019e31b941ec5366fad373ebc86
Jorge <jorge.alfmur@gmail.com>
2017-05-07 23:43:07 -0500

    archivos estaticos

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require('express')
 var app = express()
+
+//archivos static
+app.use(express.static('public'))
+
 //configuramos el motor de vista
 app.set('view engine', 'pug')
 
-app.get('/',  (req, res)=>{
+app.get('/', (req, res)=>{
   res.render('index')
+})
+
+app.get('/login', (req, res)=>{
+  res.render('login')
 })
 
 app.listen(3000, ()=>{
