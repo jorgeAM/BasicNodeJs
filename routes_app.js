8f3d16535519bad996a5ae361a6297dd8e31b3ec
Jorge <jorge.alfmur@gmail.com>
2017-05-10 14:05:11 -0500

    rutas de crud imagenes

--- routes_app.js
+++ routes_app.js
@@ -7,4 +7,33 @@ router.get('/', function (req, res) {
 	res.render('app/home')
 })
 
+/* REST */
+router.get('/imagenes/new',(req,res)=>{
+	res.render('app/imagenes/new')
+})
+
+router.get('/imagenes/:id/edit',(req,res)=>{
+
+})
+
+router.route('/imagenes/:id')
+	.get((req,res)=>{
+		//show
+	})
+	.put((req,res)=>{
+		//update
+	})
+	.delete((req,res)=>{
+		//delete
+	})
+
+	router.route('/imagenes/')
+	.get((req,res)=>{
+		//index
+
+	})
+	.post((req,res)=>{
+		//create
+	})
+
 module.exports = router
