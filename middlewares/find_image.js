5cdf7fa23f807ca9c168f7d92a0cd1cadbaceab6
Jorge <jorge.alfmur@gmail.com>
2017-05-14 17:11:29 -0500

    validar propietario de imagen

--- middlewares/find_image.js
+++ middlewares/find_image.js
@@ -1,11 +1,13 @@
 //exportamos modelo IMAGE
 const Image = require('../models/image').Image
+//llamar middleware de permisos
+const owner_check = require('./image_permission')
 
 module.exports = (req, res, next)=>{
 	Image.findById(req.params.id)
 	.populate('creator')
 	.exec((err,image)=>{
-		if (image != null){
+		if (image != null && owner_check(image, req, res)){
 			res.locals.image =  image
 			next()
 		}
---
+++ middlewares/image_permission.js
@@ -0,0 +1,15 @@
+const Image = require('../models/image').Image
+module.exports = (image,req,res)=>{
+	// True = Tienes permisos
+	// Falso = SI no tienes permisos
+	if(req.method === "GET" && req.path.indexOf("edit" ) < 0){ 
+		// Ver la imagen
+		return true
+	}
+	if(typeof image.creator == "undefined") return false
+	if(image.creator._id.toString() == res.locals.user._id){
+		// Esta imagen yo la subí
+		return true
+	}
+	return false
+}
